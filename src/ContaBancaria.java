7bebda0a15af2ba4aa3891b199d6ba8757347988
BrendoReisDev <brendo.studios.hub@gmail.com>
2025-07-31 15:28:33 -0300

    Sistema de Caixa Bancário!

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class ContaBancaria extends Transacao {
-public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-            Transacao transacao =  new Transacao();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Transacao transacao = new Transacao();
+
+        while (true) {
             System.out.println("""
-                ----------- BANCO ---------------
+                ------------ BANCO --------------
                 | [1] Entrar no Banco           |
                 | [0] Sair                      |
                 ---------------------------------
@@ -18,44 +20,53 @@ public static void main(String[] args) {
             switch (opcao) {
                 case 1:
                     Cliente cliente = new Cliente();
-                    int opcao2 = 0;
 
+                    int opcao2;
+                    do {
                         System.out.println("""
                                 ------------ BANCO --------------
                                 | [1] Sacar                     | 
                                 | [2] Depositar                 |
-                                | [3] Saldo                     |
+                                | [3] Ver Saldo                 |
                                 | [0] Sair                      |
                                 ---------------------------------
                                 Escolha uma opção: """);
                         opcao2 = scanner.nextInt();
+
                         switch (opcao2) {
                             case 1:
-                                System.out.println("Digite o valor do saque: ");
+                                System.out.print("Digite o valor do saque: ");
                                 double valorSaque = scanner.nextDouble();
                                 transacao.sacar(valorSaque);
                                 break;
                             case 2:
-                                System.out.println("Digite o valor do deposito: ");
+                                System.out.print("Digite o valor do depósito: ");
                                 double valorDeposito = scanner.nextDouble();
                                 transacao.deposito(valorDeposito);
-
+                                break;
                             case 3:
                                 transacao.mostraDados();
                                 break;
-
+                            case 0:
+                                System.out.println("Saindo da conta...");
+                                break;
+                            default:
+                                System.out.println("Opção inválida.");
                         }
 
-                  break;
+                    } while (opcao2 != 0);
+
+                    break;
+
                 case 0:
                     System.out.println("Obrigado por usar o sistema!");
+                    scanner.close();
                     System.exit(0);
                     break;
 
                 default:
-                    System.out.println("\nOpção inválida. Tente novamente.");
-            }
+                    System.out.println("Opção inválida. Tente novamente.");
             }
         }
-
-
+    }
+}
